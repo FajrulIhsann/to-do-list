a5f3b9e5edc52b81422446043013f9fc1d3e77a7
FajrulIhsann <dawnnbsns@gmail.com>
2026-05-24 17:23:39 +0700

    configure .env.example

--- connection/database.js
+++ connection/database.js
@@ -2,7 +2,8 @@ const mysql = require('mysql2')
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_HOST_PORT,
+    // Uncomment the line below if you need to configure the port
+    // port: process.env.DB_HOST_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
--- connection/init_db.js
+++ connection/init_db.js
@@ -5,7 +5,8 @@ const setDatabase = `CREATE DATABASE IF NOT EXISTS todo_list;`
 async function initDatabase() {
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST,
-        port: process.env.DB_HOST_PORT,
+        // Uncomment the line below if you need to configure the port
+        // port: process.env.DB_HOST_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         
